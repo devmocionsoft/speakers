f8924047fc0086cdaa349771e58a9894082181d8
devmocionsoft <apps@mocionsoft.com>
2018-05-02 00:06:36 -0500

    inicial

---
+++ src/providers/academy-provider.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+ 
+@Injectable()
+export class AcademyProvider {
+  reasonToJoin() {
+    return 'The Ionic Academy helps you to learn everything Ionic!';
+  }
